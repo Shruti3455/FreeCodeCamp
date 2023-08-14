30000d358c4f577e8e184f0f7b0d22451d1258e9
shruti3455 <shrutisumananand@gmail.com>
2023-08-14 10:37:47 +0530

    comment deleted

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,10 +1,5 @@
 import asyncHandler from "../middleware/asyncHandler.js";
 
-import User from "../models/userModel.js";
-
-// @desc    Auth user & get token
-// @route   POST /api/users/auth
-// @access  Public
 const authUser = asyncHandler(async (req, res) => {
   res.send("auth user");
 });
