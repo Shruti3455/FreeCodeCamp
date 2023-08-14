02a2f5fdf0eef865a9bf208e555c28a4fdafeda0
shruti3455 <shrutisumananand@gmail.com>
2023-08-14 10:54:31 +0530

    little change

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,28 +1,18 @@
-import express from 'express';
-import {
-  authUser,
-  registerUser,
-  logoutUser,
-  getUserProfile,
-  updateUserProfile,
-  getUsers,
-  deleteUser,
-  getUserById,
-  updateUser,
-} from '../controllers/userController.js';
-import { protect, admin } from '../middleware/authMiddleware.js';
+import express from "express";
+import { authUser } from "../controllers/userController.js";
+import { protect, admin } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.route('/').post(registerUser).get(protect, admin, getUsers);
-router.post('/auth', authUser);
-router.post('/logout', logoutUser);
+router.route("/").post(registerUser).get(protect, admin, getUsers);
+router.post("/auth", authUser);
+router.post("/logout", logoutUser);
 router
-  .route('/profile')
+  .route("/profile")
   .get(protect, getUserProfile)
   .put(protect, updateUserProfile);
 router
-  .route('/:id')
+  .route("/:id")
   .delete(protect, admin, deleteUser)
   .get(protect, admin, getUserById)
   .put(protect, admin, updateUser);
